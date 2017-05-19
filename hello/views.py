668eabf8de252614d710576f51c823ac080011f3
Ignacio Avas <gmail@ignacioavas.com>
2017-05-18 21:19:09 -0300

    Added a nice view to visualize Locations

--- hello/views.py
+++ hello/views.py
@@ -1,20 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
 
-from .models import Greeting
+from .models import GeoLocation
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from Python!')
+	locations = GeoLocation.objects.all()
 
-def db(request):
+	encoded_locations = json.dumps([
+		{ 'lat': float(location.lat), 
+			'lng': float(location.lng), 
+			'when': location.when.isoformat()
+		}
+		for location in locations
+	])
 
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, 'db.html', {'greetings': greetings})
+	return render(request, 'db.html', {'locations': locations, 'encoded_locations': encoded_locations })
 
 def upload(request):
-	return HttpResponse('')
+	geolocation = GeoLocation()
+	geolocation.lat = request.GET['lat']
+	geolocation.lng = request.GET['lng']
+	geolocation.save()
+
+	return HttpResponse('Got from you lat={}, lng={}, time={}'.format(
+		geolocation.lat,
+		geolocation.lng,
+		geolocation.when
+		)
+	)
